5687f19f56db4f1ec738aca07c044678bce5359e
aol1306 <aol1306@protonmail.ch>
2017-08-04 19:35:34 +0200

    Init

---
+++ main.py
@@ -0,0 +1,25 @@
+import window_hide
+import socket
+
+DEBUG = True
+
+def init():
+	if DEBUG == False:
+		window_hide.hide_console_window()
+
+def try_connect_and_handle():
+	# connect to remote server
+	# on success: pass execution to handler
+	pass
+		
+def main_loop():
+	print("Enter main loop")
+	while True:
+		try_connect_and_handle()
+		
+def main():
+	init()
+	main_loop()
+
+if __name__ == "__main__":
+	main()

8f3092f25658397f9a0c14fc5a4f5bc554903b50
aol1306 <aol1306@protonmail.ch>
2017-08-04 21:49:53 +0200

    Add shell

--- CommandHandler.py
+++ CommandHandler.py
@@ -1,6 +1,32 @@
+import subprocess
+
 class CommandHandler:
+    def __init__(this):
+        this.state = "normal"
+        print("state: normal")
+
     def handle_command(this, command):
-        print("Handling command", command)
+        if this.state == "normal":    
+            return this.handle_normal_command(command)
+        elif this.state == "shell":
+            return this.handle_shell_command(command)
+        
+    def handle_normal_command(this, command):
+        print("Handling normal command", command)
         if command == "exit":
             exit()
-        return "ACK"
+        if command == "shell":
+            this.state = "shell"
+            print("state: shell")
+            return "Switched to shell"
+        else:
+            return "Unknown command"
+            
+    def handle_shell_command(this, command):
+        print("Handling shell command", command)
+        if command == "exit":
+            this.state = "normal"
+            print("state: normal")
+            return "exit"
+        else:
+            return str(subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True))

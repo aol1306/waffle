1f5cf30ddbe014aa131d1044be15b38c511fc798
aol1306 <aol1306@protonmail.ch>
2017-08-04 19:41:11 +0200

    Tabs to spaces

--- window_hide.py
+++ window_hide.py
@@ -1,12 +1,12 @@
 import win32gui
 
 def enum_windows_callback(hwnd, windowList):
-	text = win32gui.GetWindowText(hwnd)
-	className = win32gui.GetClassName(hwnd)
-	if className == "ConsoleWindowClass":
-		windowList.append((hwnd, text, className))
+    text = win32gui.GetWindowText(hwnd)
+    className = win32gui.GetClassName(hwnd)
+    if className == "ConsoleWindowClass":
+        windowList.append((hwnd, text, className))
 
 def hide_console_window():
-	windows = []
-	win32gui.EnumWindows(enum_windows_callback, windows)
-	win32gui.ShowWindow(windows[0][0], False)
+    windows = []
+    win32gui.EnumWindows(enum_windows_callback, windows)
+    win32gui.ShowWindow(windows[0][0], False)

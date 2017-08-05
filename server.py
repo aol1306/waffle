57a2349ddd5750ef0d6b57c7b59890c67e4ec694
aol1306 <aol1306@protonmail.ch>
2017-08-05 19:33:54 +0200

    Server is threaded now

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
 import connection
+import threading
 
-def main_loop(conn):
-    try:
-        while True:
-            msg = input("> ")
-            conn.send_one_string_message(msg)
-            recv = conn.recv_one_string_message() 
-            print(recv)
-            if msg == "exit":
-                conn.close()
-                break
-    except:
-        print("Connection closed")
-        conn.close()
+class ServerHandler(threading.Thread):
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.running = True
+        self.conn = conn
+        
+    def run(self):
+        try:
+            while self.running:
+                msg = input("> ")
+                self.conn.send_one_string_message(msg)
+                recv = self.conn.recv_one_string_message() 
+                print(recv)
+                if msg == "exit":
+                    self.conn.close()
+                    self.running = False
+        except:
+            print("Connection closed")
+            self.conn.close()
         
 sock = connection.server_factory(5555)
 print(sock)
@@ -25,4 +32,5 @@ while True:
     conn = connection.Connection(clientsocket)
     # get hello message
     conn.recv_one_string_message()
-    main_loop(conn)
+    server_handler = ServerHandler(conn)
+    server_handler.start()
